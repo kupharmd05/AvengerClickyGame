d4fe7ab7c053902bb81c79874a0e8fa6cb9bbbda
kupharmd05 <kupharmd05@gmail.com>
2019-04-12 20:38:35 -0500

    Updates

---
+++ precache-manifest.52b69af123959f3eb8c3b94b61596114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b3663f05c27a7e82b0e",
+    "url": "/AvengerClickyGame/static/js/runtime~main.8551008e.js"
+  },
+  {
+    "revision": "928954b64cf518b31be3",
+    "url": "/AvengerClickyGame/static/js/main.d22bae5b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/AvengerClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "928954b64cf518b31be3",
+    "url": "/AvengerClickyGame/static/css/main.8752fbad.chunk.css"
+  },
+  {
+    "revision": "4e4d08de5abc07af9876a1830a19fe5d",
+    "url": "/AvengerClickyGame/index.html"
+  }
+];
